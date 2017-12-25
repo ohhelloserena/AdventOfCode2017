ab0f3b89c45297c535e17151474a5f3bdf145bcd
ohhelloserena <ohhelloserena@gmail.com>
2017-12-24 23:57:28 -0800

    Day 7, Part 2 sort of

--- Day7/src/Main.java
+++ Day7/src/Main.java
@@ -1,12 +1,23 @@
+import com.sun.xml.internal.messaging.saaj.soap.FastInfosetDataContentHandler;
+
+import javax.sound.midi.SysexMessage;
 import javax.swing.tree.TreeNode;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
+
 
 public class Main {
 
+    public static String arrow = "->";
+    public static String[] arr;
+    public static List<String> path;
+
+
     public static void main(String[] args) throws FileNotFoundException {
-        //System.out.println("Hello World!");
+
+        path = new ArrayList<>();
 
         Scanner sc = new Scanner(new File("src/test_input"));
         List<String> lines = new ArrayList<String>();
@@ -14,7 +25,7 @@ public class Main {
             lines.add(sc.nextLine());
         }
 
-        String[] arr = lines.toArray(new String[0]);
+        arr = lines.toArray(new String[0]);
 
         // Part 1
 
@@ -24,15 +35,11 @@ public class Main {
 
         for (int i = 0; i < arr.length; i++) {
             String program = arr[i];
-            String arrow = "-> ";
-           
 
             if (program.contains(arrow)) {
-                String parentSubstring = program.substring(0, program.indexOf(" ("));
+                String parentSubstring = program.substring(0, program.indexOf("(") - 1);
                 parents.add(parentSubstring);
-
-                String childrenSubstring = program.substring(program.indexOf(arrow) + arrow.length(), program.length());
-                String[] child = childrenSubstring.split(", ");
+                String[] child = getSubstring(program);
 
                 for (int j = 0; j < child.length; j++) {
                     children.add(child[j]);
@@ -51,14 +58,135 @@ public class Main {
 
         // Part 2
 
-        for (int m = 0; m < arr.length; m++) {
+        List<Integer> weightList = new ArrayList<>();
+        List<String> programsList = new ArrayList<>();
 
-            
+
+        String rootPath = "";
+
+
+        // set weightList and programsList
+        for (int m = 0; m < arr.length; m++) {
+            String program = arr[m];
+            String strWeight = program.substring(program.indexOf("(") + 1, program.indexOf(")"));
+            String pr = program.substring(0, program.indexOf("(") - 1);
+            programsList.add(pr);
+            int weight = Integer.parseInt(strWeight);
+            weightList.add(weight);
         }
 
-        
+        List<String> rootChildren = getChildren(root);
+        List<Integer> sumList = new ArrayList<>();
+        List<Integer> tempWeights = new ArrayList<>();
 
-        
+        for (String r : rootChildren) {
+            int sum = 0;
+            List<String> results = findAllChildren(r);
+            results.add(r);
+
+            for (String result : results) {
+                String[] breakdown = result.split(",");
+
+                for (String b : breakdown) {
+                    int index = programsList.indexOf(b);
+                    int weight = weightList.get(index);
+                    tempWeights.add(weight);
+                    sum += weight;
+                }
+            }
+            sumList.add(sum);
+            int iSum = sumList.indexOf(sum);
+
+
+            if (iSum != 0) {
+                int prevSum = sumList.get(iSum - 1);
+
+                if (sum != prevSum) {
+                    System.out.println(tempWeights);
+                    break;
+                }
+            }
+            results.clear();
+            tempWeights.clear();
+        }
+
+        System.out.println("sumList: " + sumList.toString());
+
+
     }
+
+    /**
+     * Return the portion after the ->
+     *
+     * @param str
+     * @return
+     */
+    public static String[] getSubstring(String str) {
+        String arrow = "-> ";
+        String childrenSubstring = str.substring(str.indexOf(arrow) + arrow.length(), str.length());
+        String[] subStrings = childrenSubstring.split(", ");
+        return subStrings;
+    }
+
+    /**
+     * Return the children for a given program
+     *
+     * @param program
+     * @return
+     */
+    public static List<String> findAllChildren(String program) {
+        List<String> arrList = Arrays.asList(arr);
+        List<String> tempPath = new ArrayList<>();
+        List<String> temp = new ArrayList<>();
+
+        for (String a : arrList) {
+            boolean doesMatch = a.substring(0, a.indexOf("(") - 1).contains(program);
+            boolean hasChildren = a.contains(arrow);
+
+            if (doesMatch && hasChildren) {
+                String[] pathChildren = getSubstring(a);
+                for (String p : pathChildren) {
+                    temp.add(p);
+                }
+                break;
+            }
+        }
+
+        if (temp.size() > 0) {
+            path.addAll(temp);
+            tempPath.addAll(temp);
+            temp.clear();
+
+            for (String t : tempPath) {
+                findAllChildren(t);
+            }
+        } else {
+            return path;
+        }
+        return path;
+    }
+
+    public static List<String> getChildren(String program) {
+        List<String> arrList = Arrays.asList(arr);
+        List<String> temp = new ArrayList<>();
+
+        for (String a : arrList) {
+            boolean doesMatch = a.substring(0, a.indexOf("(") - 1).contains(program);
+            boolean hasChildren = a.contains(arrow);
+
+            if (doesMatch && hasChildren) {
+                String[] pathChildren = getSubstring(a);
+                for (String p : pathChildren) {
+                    temp.add(p);
+                }
+                break;
+            }
+        }
+
+        return temp;
+    }
+
+
 }
+
 
